ef41607bae2631a424216b8c93aeaf4ec4128575
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-27 13:45:53 -0600

    Refactor braille processing

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -60,31 +60,48 @@ braille_dictionary = {
   ',' => ['..','0.','..']
 }
 
-character_array = File.open(file_array[0]).each_char.map do |byte|
-  byte
+word_array = File.read(file_array[0]).split
+
+max_width = 40
+final_result = []
+string_element = ''
+word_array.each do |word|
+  if word.length > max_width
+    final_result.push(string_element) unless string_element.empty?
+    string_element = ''
+    word.each_char do |character|
+      string_element += character
+      if string_element.length == max_width
+        final_result.push(string_element)
+        string_element = ''
+      end
+    end
+  elsif (string_element + " " + word).length <= max_width
+    string_element += (" " + word)
+  else
+    final_result.push(string_element)
+    string_element = word
+  end
+end
+final_result.push(string_element)
+
+braille_array = []
+array_builder = []
+final_result.each do |line|
+  line.split('').each do |character|
+    array_builder.push(braille_dictionary[character])
+  end
+  braille_array.push(array_builder)
+  array_builder = []
 end
 
-character_array.delete("\n")
-
-braille_array_line1 = []
-braille_array_line2 = []
-braille_array_line3 = []
-character_array.each do |character|
-  braille_array_line1.push(braille_dictionary[character][0])
-  braille_array_line2.push(braille_dictionary[character][1])
-  braille_array_line3.push(braille_dictionary[character][2])
+braille_array.each do |element|
+  new_array = element.transpose
+  File.open(file_array[1], "a") { |f|
+    f << "#{new_array[0].join('')}\n"
+    f << "#{new_array[1].join('')}\n"
+    f << "#{new_array[2].join('')}\n"
+  }
 end
 
-braille_hash = {
-  braille_line1: braille_array_line1,
-  braille_line2: braille_array_line2,
-  braille_line3: braille_array_line3
-}
-
-File.open('braille.txt', "w") do |f|
-  f << "#{braille_hash[:braille_line1].join("")}\n"
-  f << "#{braille_hash[:braille_line2].join("")}\n"
-  f << "#{braille_hash[:braille_line3].join("")}\n"
-end
-
-puts "Created #{file_array[1]} containing #{character_array.length} characters"
+print "Created #{file_array[1]} containing #{file_array[1].length} characters"
