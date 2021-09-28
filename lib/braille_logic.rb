cdb359e0577d283d94de26a3fd0a2887c557458e
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-27 20:19:02 -0600

    Create BrailleLogic class

---
+++ lib/braille_logic.rb
@@ -0,0 +1,18 @@
+class BrailleLogic
+  attr_reader :read_file,
+              :write_file,
+              :converter
+
+  def initialize(file_array)
+    @read_file = file_array[0]
+    @write_file = file_array[1]
+    @converter = BrailleConverter.new(@read_file, @write_file)
+  end
+
+  def braille_conversion_logic
+    english_word_array = @converter.create_word_array
+    english_line_array = @converter.create_line_array(english_word_array, 40)
+    braille_line_array = @converter.create_braille_array(english_line_array)
+    @converter.print_braille_to_file(braille_line_array)
+  end
+end
